ea735398f58ee989a24e20751083e61e351fb7ac
Kamil Lewan <carmel4a97@gmail.com>
2019-02-15 18:00:57 +0100

    Cleanup `loadFile` methods

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"path/filepath"
 	"strconv"
 )
 
@@ -23,24 +24,19 @@ func loadFilesRecursively(files *map[string]string, cwd string) {
 		if f.IsDir() {
 			loadFilesRecursively(files, cwd+f.Name()+"/")
 		} else {
-			fmt.Println(cwd + f.Name())
-			loadFileFromFullPath(files, cwd+f.Name())
+			basename := f.Name()[0 : len(f.Name())-len(filepath.Ext(f.Name()))]
+			fmt.Println(basename)
+			fmt.Println(cwd + filepath.Base(f.Name()))
+			loadFile(files, cwd+basename, filepath.Ext(f.Name()))
 		}
 	}
 }
 
-func loadFileFromFullPath(files *map[string]string, fullPath string) {
-	dat, err := ioutil.ReadFile(fullPath)
-	check(err)
-	(*files)[fullPath] = string(dat)
-
-	// dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
-	// fmt.Print("Path: ", dir)
-}
-
 func loadFile(files *map[string]string, path string, ext string) {
-	loadFileFromFullPath(files, "./src/"+path[1:]+"."+ext)
-
+	dat, err := ioutil.ReadFile(path + ext)
+	check(err)
+	// hack for now
+	(*files)[path[5:]] = string(dat)
 }
 
 func getFile(files *map[string]string, path string) string {
@@ -55,12 +51,9 @@ func main() {
 
 	loadFilesRecursively(&files, "./src/")
 
-	loadFile(&files, "/index", "html")
-	loadFile(&files, "/404", "html")
-	loadFile(&files, "/favicon.ico", "png")
+	loadFile(&files, "./src/favicon.ico", ".png")
 
 	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
-		// fmt.Fprintf(rw, "Hello, %q", html.EscapeString(r.URL.Path))
 		fmt.Fprintf(rw, getFile(&files, html.EscapeString(r.URL.Path)))
 		fmt.Println("Recivied request: ", html.EscapeString(r.URL.Path))
 	})
